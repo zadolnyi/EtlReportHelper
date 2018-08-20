b3343874ca97964193fe623ee2f469d7d4faf35f
zadol <zadolnyi@rambler.ru>
2018-08-20 21:40:25 +0300

    последнее изменение в master

--- src/main/java/com/zadolnyi/reporthelper/JPAConfig.java
+++ src/main/java/com/zadolnyi/reporthelper/JPAConfig.java
@@ -32,7 +32,7 @@ public class JPAConfig {
                 .build();
     }
 
-//    @Bean(name = "entityManagerFactory") (+ изменение в master)
+//    @Bean(name = "entityManagerFactory") (+ изменение в master)(+ последнее изменение в master)
 //    @Primary
 //    public LocalContainerEntityManagerFactoryBean entityManagerFactoryBean() {
 //        LocalContainerEntityManagerFactoryBean entityManagerFactoryBean = new LocalContainerEntityManagerFactoryBean();
