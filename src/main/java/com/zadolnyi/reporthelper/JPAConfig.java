7e431ee56c669dbe647c017614973699616a1bf5
zadol <zadolnyi@rambler.ru>
2018-08-20 21:29:09 +0300

    (+ последнее изменение

--- src/main/java/com/zadolnyi/reporthelper/JPAConfig.java
+++ src/main/java/com/zadolnyi/reporthelper/JPAConfig.java
@@ -32,7 +32,7 @@ public class JPAConfig {
                 .build();
     }
 
-//    @Bean(name = "entityManagerFactory") (+ изменение в VitBranch2)(+ новые изменения)(+ изменение в master)
+//    @Bean(name = "entityManagerFactory") (+ изменение в VitBranch2)(+ новые изменения)(+ изменение в master)(+ последнее изменение)
 //    @Primary
 //    public LocalContainerEntityManagerFactoryBean entityManagerFactoryBean() {
 //        LocalContainerEntityManagerFactoryBean entityManagerFactoryBean = new LocalContainerEntityManagerFactoryBean();
