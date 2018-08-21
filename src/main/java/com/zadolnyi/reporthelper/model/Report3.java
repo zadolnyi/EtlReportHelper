a59a1fe523c22945ba5903fa8e93b4d158145d85
zadol <zadolnyi@rambler.ru>
2018-08-21 16:51:48 +0300

    Создал разные БД для источника и цели, добавил третий отчёт

--- src/main/java/com/zadolnyi/reporthelper/model/Report3.java
+++ src/main/java/com/zadolnyi/reporthelper/model/Report3.java
@@ -12,37 +12,21 @@ import java.sql.SQLException;
 @Data
 @NoArgsConstructor
 @Entity
-@Table(name="report3", schema="public")
+@Table(name="report3", schema="target_schema")
 public class Report3 implements ReportLine {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-    private String num;
-    @Column(name = "origin_order_num")
-    private String originOrderNum;
-    @Column(name = "order_date")
-    private String orderDate;
-    private String cost;
-    @Column(name = "return_of_documents")
-    private String returnOfDocuments;
-    @Column(name = "payment_status")
-    private String paymentStatus;
-    @Column(name = "client_id")
-    private Long clientId;
-    @Column(name = "client_name")
-    private String clientName;
+    @Column(name = "column_2")
+    private String column2;
+    @Column(name = "column_5")
+    private Integer column5;
 
     @Override
     public ReportLine mapEntry(ResultSet rs) throws SQLException {
         //setId(rs.getLong("id"));
-        this.setNum(rs.getString("num"));
-        setOriginOrderNum(rs.getString("origin_order_num"));
-        setOrderDate(rs.getString("order_date"));
-        setCost(rs.getString("cost"));
-        setReturnOfDocuments(rs.getString("return_of_documents"));
-        setPaymentStatus(rs.getString("payment_status"));
-        setClientId(rs.getLong("client_id"));
-        setClientName(rs.getString("client_name"));
+        this.setColumn2(rs.getString("column_2"));
+        this.setColumn5(rs.getInt("column_5"));
         return this;
     }
 
@@ -52,14 +36,8 @@ public class Report3 implements ReportLine {
             @Override
             public void setValues(PreparedStatement ps, Report3 report) throws SQLException {
                 //ps.setLong(1, report.getId());
-                ps.setString(1, report.getNum());
-                ps.setString(2, report.getOriginOrderNum());
-                ps.setString(3, report.getOrderDate());
-                ps.setString(4, report.getCost());
-                ps.setString(5, report.getReturnOfDocuments());
-                ps.setString(6, report.getPaymentStatus());
-                ps.setLong(7, report.getClientId());
-                ps.setString(8, report.getClientName());
+                ps.setString(1, report.getColumn2());
+                ps.setInt(2, report.getColumn5());
             }
         };
     }
